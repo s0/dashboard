b7b6dbad08639a73dd80b939e9bf03d5a5645000
Sam Lanning <sam@samlanning.com>
2014-09-24 14:45:54 +0100

    Added card animations

--- static/js/main.js
+++ static/js/main.js
@@ -1,9 +1,32 @@
 (function($){
 
     $(document).ready(function(){
+
+
+        // Card Manipulation
+        function hide_card($card){
+            $card.css({height: 0, opacity: 0})
+        }
+
+        function open_card($card){
+            var $inner = $card.children(".inner.active:first")
+            $card.animate({height: $inner.outerHeight(true)}, function(){
+                $card.css("height", "auto")
+                $card.animate({opacity: 1})
+            })
+        }
+
+
+
+        $(".card.closed").each(function(){
+            hide_card($(this))
+            open_card($(this))
+        })
+
+
+
+
         console.debug($(document).width())
     })
-
-
 
 })(jQuery);
