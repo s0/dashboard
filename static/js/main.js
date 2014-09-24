4e144a7c51f388de197bf0eb08d58972449cbbad
Sam Lanning <sam@samlanning.com>
2014-09-24 15:18:17 +0100

    Added scrolling / overflow behaviour for cards

--- static/js/main.js
+++ static/js/main.js
@@ -2,6 +2,31 @@
 
     $(document).ready(function(){
 
+        var msg_id = 0,
+            $window = $(window),
+            $cards_top = $('.cards.top:first'),
+            $cards_bottom = $('.cards.bottom:first');
+
+        // Cards Overflow / Scroll
+
+        function update_top_cards_scroll_indicators($cards){
+            console.log(msg_id++)
+        }
+
+        $('.cards').scroll(function(){
+            update_top_cards_scroll_indicators($(this))
+        })
+
+
+        function resize_top_cards() {
+            var new_h = $window.height() - $cards_bottom.height() - 20;
+            $cards_top.height(new_h);
+            update_top_cards_scroll_indicators($cards_top)
+        }
+
+        $(window).resize(resize_top_cards)
+
+
 
         // Card Manipulation
         function hide_card($card){
@@ -10,9 +35,18 @@
 
         function open_card($card){
             var $inner = $card.children(".inner.active:first")
-            $card.animate({height: $inner.outerHeight(true)}, function(){
-                $card.css("height", "auto")
-                $card.animate({opacity: 1})
+            $card.animate({height: $inner.outerHeight(true)},
+                          {step: resize_top_cards,
+                           complete: function(){
+                               $card.css("height", "auto")
+                               $card.animate({opacity: 1})
+                           }})
+        }
+
+        function close_card($card){
+            $card.animate({opacity: 0}, function(){
+                $card.animate({height: 0},
+                              {step: resize_top_cards})
             })
         }
 
@@ -22,6 +56,12 @@
             hide_card($(this))
             open_card($(this))
         })
+
+        setTimeout(function(){
+            $(".card.closer").each(function(){
+                close_card($(this))
+            })
+                }, 1000)
 
 
 
